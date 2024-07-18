0fcf806357b50583011f8268f73253b2792efc80
Tenzin Kunchok <tenkunchok012511@gmail.com>
2024-07-18 10:33:17 -0400

    categories section completed and styled

--- src/app/_components/Categories/index.tsx
+++ src/app/_components/Categories/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Link from 'next/link'
 
-const Categories = () => {
+import { Category } from '../../../payload/payload-types'
+import CategoryCard from './CategoryCard'
+
+import classes from './index.module.scss'
+
+const Categories = ({ categories }: { categories: Category[] }) => {
   return (
-    <div>
-      Categories
-    </div>
+    <section className={classes.container}>
+      <div className={classes.titleWrapper}>
+        <h3>Shop by Categories</h3>
+        <Link href="/products">Show All</Link>
+      </div>
+
+      <div className={classes.list}>
+        {categories.map(category => {
+          return <CategoryCard key={category.id} category={category} />
+        })}
+      </div>
+    </section>
   )
 }
 
